65dab357c8dcfc15e3d6bacfffe1bea1f14e5894
sauravrawat99 <rawatsaurav145@gmail.com>
2026-01-09 10:48:53 +0530

    start the code

---
+++ basic/basic1.java
@@ -0,0 +1,15 @@
+//what is jav
+// java is a object oriented platform independent programming language
+
+//| Component | Work                                  |
+//        | --------- | ------------------------------------- |
+//        | **JVM**   | Java code run karne ke liye           |
+//        | **JRE**   | JVM + libraries (run karne ke liye)   |
+//        | **JDK**   | JRE + compiler tools (banane ke liye) |
+//
+
+public   class basic{
+    public static void main(String[]args){
+    system.out.println("hello world")
+    }
+}
